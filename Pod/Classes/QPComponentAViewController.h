e453bd743d41a431a82587b3517c7037036a51ac
gavin heer <gavin.heer@quickmobile.com>
2014-07-03 15:50:19 -0700

    adding test vc

---
+++ Pod/Classes/QPComponentAViewController.h
@@ -0,0 +1,13 @@
+//
+//  QPComponentAViewController.h
+//  ComponentA
+//
+//  Created by Gavin Heer on 2014-07-03.
+//  Copyright (c) 2014 gavin heer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QPComponentAViewController : UIViewController
+
+@end
---
+++ Pod/Classes/QPComponentAViewController.m
@@ -0,0 +1,38 @@
+//
+//  QPComponentAViewController.m
+//  ComponentA
+//
+//  Created by Gavin Heer on 2014-07-03.
+//  Copyright (c) 2014 gavin heer. All rights reserved.
+//
+
+#import "QPComponentAViewController.h"
+
+@interface QPComponentAViewController ()
+
+@end
+
+@implementation QPComponentAViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
